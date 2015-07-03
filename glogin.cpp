42bdc5b3d413e38c15f1f50d24ffac36be39cb29
Michael Gentili <gentili@mcpnet.ca>
2015-07-02 20:33:18 -0400

    Works now

--- glogin.cpp
+++ glogin.cpp
@@ -7,7 +7,23 @@
 //============================================================================
 
 #include <SDL.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_SetVideoMode(640,480,32,SDL_DOUBLEBUF);
+	SDL_Event event;
+	bool isRunning = true;
+	cout << "start" << endl;
+	while(isRunning){
+		if(SDL_PollEvent(&event) && event.type == SDL_QUIT){
+			cout << "quit" << endl;
+			isRunning=false;
+		}
+	}
+	cout << "done" << endl;
+	SDL_Quit();
 	return 0;
 }
